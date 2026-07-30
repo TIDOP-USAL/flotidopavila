4a62cfbc2c15461d209d9155eb9fe3e7d3f0f72f
gustavo1976 <gustavo1976@gmail.com>
2026-07-30 10:10:13 +0200

    modifico app

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def pv_modules():
         return jsonify({"error": "Ocurrió un error inesperado", "mensaje": str(e)}), 500
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5003))
-    app.run(host='0.0.0.0', port=port, debug=True
+    app.run(host='0.0.0.0', port=port, debug=True)
